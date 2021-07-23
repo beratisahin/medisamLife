d5e1a65240f266f24cdaa59bbaac408766be9916
beratisahin <beratisahin94@gmail.com>
2021-07-24 00:37:36 +0300

    v1.7

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -11,7 +11,7 @@ export default class Services extends Component {
            return (
 
                    <div>
-                        <div className="col-12 m-4 pl-5 pr-5 pt-2 pb-2 card-temp" style={{backgroundColor:"#FFEFFB",borderRadius:"1rem",textAlign:"center",height:"40rem",border:"3px #C92798 solid"}}>
+                        <div className="col-12 m-4 pl-5 pr-5 pt-2 pb-2 card-temp" style={{backgroundColor:"#BCDEE6",borderRadius:"1rem",textAlign:"center",height:"40rem",border:"3px #BCDEE6 solid"}}>
                                 <img src={photo} className="card-photo" alt="..." style={{width:"100%", height:"20rem",padding:"1%",borderRadius:"1rem"}}></img>
                                 <p class="card-title" style={{color:"#C92798",fontSize:"large",marginTop:"1rem"}}>{title}</p>
                                 <p class="card-text" style={{fontSize:"small",textAlign:"justify"}}>{text}</p>
---
+++ src/components/test/Test.js
@@ -0,0 +1,30 @@
+import './Test.css';
+import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+
+
+export default class Test extends Component {
+      
+    render() {
+           //Destructing
+           const {photo,title,text}=this.props;
+           return (
+
+                   <div>
+                        <div className="col-12 m-4 pl-5 pr-5 pt-2 pb-2 card-temp" style={{backgroundColor:"#F1F1F1",borderRadius:"1rem",textAlign:"center",height:"40rem",border:"3px #3A667D inset"}}>
+                                <img src={photo} className="card-photo" alt="..." style={{width:"100%", height:"20rem",padding:"1%",borderRadius:"1rem"}}></img>
+                                <p class="card-title" style={{color:"#3A667D",fontSize:"large",marginTop:"1rem"}}>{title}</p>
+                                <p class="card-text" style={{fontSize:"small",textAlign:"justify"}}>{text}</p>
+                                
+                                
+
+                        </div>
+
+                   </div>
+
+
+           
+
+           )
+    }
+}
