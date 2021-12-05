7b8e82d527ddae93dfa4e2a8dbfa65d8a38e2640
beratisahin <beratisahin94@gmail.com>
2021-12-05 16:18:38 +0300

    doctor image height was increased

--- src/components/kurumsal/ekibimiz/Ekibimiz.js
+++ src/components/kurumsal/ekibimiz/Ekibimiz.js
@@ -5,33 +5,10 @@ import React from 'react';
 import useDocumentTitle from '../../../useDocumentTitle';
 
 function Ekibimiz() {
-    useDocumentTitle('Ekibimiz - Bercislina Epilasyon ve Güzellik Merkezi');
+    useDocumentTitle('Ekibimiz - Medisam Ağız ve Diş Sağlığı');
     return (
-        <div className="row mr-4" style={{marginLeft:"10px"}} >
-        <div style={{marginTop:"10px",marginBottom:"10px",justifyContent:"flex-end !important"}}>
-            <h3 style={{textAlign:"center",color:"#5FA5A0"}}><b>Ekibimiz</b></h3>
-            <div className="col-lg-3 col-md-6 col-sm-12 col-12 ekiptemp" >
-                <img src={erkek} className="d-block img-responsive w-100 ekippho" title="temizlik" alt="temizlik" ></img>
-                <p class="ekip-text">1- Dt. Enes Burak Uğurlu </p>
-           
-            </div>
-            <div className="col-lg-3 col-md-6 col-sm-12 col-12 ekiptemp" >
-                <img src={erkek} className="d-block img-responsive w-100 ekippho" title="ilaçlama" alt="koltuk-yıkama" ></img>
-                <p class="ekip-text">2- Dt. Hayati Şahin  </p>
-            </div>
-            <div className="col-lg-3 col-md-6 col-sm-12 col-12 ekiptemp" >
-                <img src={erkek} className="d-block img-responsive w-100 ekippho" title="merdiven" alt="merdiven"></img>
-                <p class="ekip-text">3- Dt. Emre Atalay</p>
-            </div>
-            <div className="col-lg-3 col-md-6 col-sm-12 col-12 ekiptemp" >
-                <img src={erkek} className="d-block img-responsive w-100 ekippho" title="araç-yıkama" alt="araç yıkama"></img>
-                <p class="ekip-text" >4- Dt. Resul Yüksek</p>
-            </div>
-            
-           
-
-
-        </div>
+        <div className="row">
+       
         </div>
     )
 }
