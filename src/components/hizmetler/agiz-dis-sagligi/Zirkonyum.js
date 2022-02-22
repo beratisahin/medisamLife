019d184ff8b81bba83131d2948e54f8e5d2e0b83
beratisahin <beratisahin94@gmail.com>
2022-02-23 01:32:58 +0300

    v1234

--- src/components/hizmetler/agiz-dis-sagligi/Zirkonyum.js
+++ src/components/hizmetler/agiz-dis-sagligi/Zirkonyum.js
@@ -15,42 +15,42 @@ export default function Zirkonyum() {
                 <div className="card-body col-12" style={{padding:"3rem"}}>
                     <br/>
                     
-        <h3>Zirkonya Diş Nedir?</h3>
+        <h3><strong>Zirkonya Diş Nedir?</strong></h3>
 
 <p>Beyaz renkli sert bir metal olan Zirkonyum elementi, diş hekimliğinde “zirkonya (zirkonyumdioksit)” şeklinde kullanılır. Kırılma direnci çok yüksek, biyouyumlu bir materyaldir. Nikel-titanyum gibi metal elementlerine alerjisi olan hastalar için de uygun bir seçimdir.&nbsp;</p>
 
 
-<h3>Zirkonya Tedavisi Hangi Diş Tedavilerinde Kullanılır?</h3>
+<h3><strong>Zirkonya Tedavisi Hangi Diş Tedavilerinde Kullanılır?</strong></h3>
 
 <p>Işık geçirgenliği kazımayla işlenebilir ve cilalanabilir yapısı sayesinde ön/arka tüm restorasyonların yapımında kullanılabilir. Doku dostu olduğu içinde diş hekimliğinde tercih edilen restorasyon malzemelerinden birisidir.&nbsp;</p>
 
 
-<h3>Zirkonya Kaplamaların Bakımı Nasıl Yapılır?</h3>
+<h3><strong>Zirkonya Kaplamaların Bakımı Nasıl Yapılır?</strong></h3>
 
 <p>Zirkonya kaplama bakımının normal dişlerden bir farkı yoktur. Günde 2 kez fırçalanmalı, her dişin arası diş ipiyle düzenli olarak temizlenmelidir. Her hasta gibi zirkonya kaplamaya sahip hastalar da 6 ayda 1 rutin diş kontrollerini ihmal etmemelidir.&nbsp;</p>
 
-<h3>Estetik Açıdan Zirkonya Dişlerin Avantajları Nelerdir?</h3>
+<h3><strong>Estetik Açıdan Zirkonya Dişlerin Avantajları Nelerdir?</strong></h3>
 
 <p>Zirkonyum kaplamaya göre daha eski bir yöntem olan metal destekli porselenlerde kaplamaların iç yüzeyi gri renkli metallerle kaplıdır. Metallerin üzerine diş renginde porselenler yığılarak bu kaplamalar elde edilir. Fakat porselenin transparan yapısı gereği altındaki gri renkli metalin silüeti zamanla belli olmaya ve kaplamalar doğal dişlere göre daha mat görünme meydana gelir. Zirkonya dişlerse tek parça yapıdadır ve tamamı dişle aynı renktedir. Ayrıca doğal dişlerdeki gibi uçlarına şeffaflık efekti verilerek kişinin kendi diş yapısına uygun bir estetik görüntü sağlanmış olur.</p>
 
-<h3>Zirkonyum Kaplama Diş Kaç Günde Yapılır?</h3>
+<h3><strong>Zirkonyum Kaplama Diş Kaç Günde Yapılır?</strong></h3>
 
 <p>Gelişen teknoloji sayesinde, dişlerin ölçüsünü dijital olarak alıp anında laboratuvara iletilir ve tasarımlar dakikalar içinde dijital ortamda yapılır. Bu sayede ölçünün alçıya dönüştürülmesi ve kurye süresi gibi zaman kayıplarından kazanç sağlanır. Öncelerde 4-5 günü bulan bu süreler artık sabah alınan ölçünün akşam üzeri teslimine kadar kısalmıştır.</p>
 
 
-<h3>Zirkonya Kaplama Sonrası Nelere Dikkat Etmeliyim?</h3>
+<h3><strong>Zirkonya Kaplama Sonrası Nelere Dikkat Etmeliyim?</strong></h3>
 
 <p>Kaplama sonrası günlük ağız hijyeni kurallarına uyulup, 6 ayda 1 diş hekimi kontrolüne gittikten sonra ekstra bir işleme ya da önleme gerek yoktur. Zirkonya diş kaplamalar kendi dişlerimiz gibi basınca dayanıklı olup kesme/çiğneme vb. günlük ihtiyaçlar yapılabilir.&nbsp;</p>
 
-<h3>Metale Karşı Alerjisi Olanlarda Zirkonya Kaplama Diş Tedavisi Uygulanabilir Mi?</h3>
+<h3><strong>Metale Karşı Alerjisi Olanlarda Zirkonya Kaplama Diş Tedavisi Uygulanabilir Mi?</strong></h3>
 
 <p>Eski tip metal destekli diş kaplamalarında alerji sık rastlanan bir durum idi. Zirkonyum alerjisi ise metal destekli kaplamaya oranla hastadan hastaya değişmekle birlikte oldukça nadir görünür. Bu nedenle zirkonyalar alerjik bireyler için öncelikli bir seçenektir.&nbsp;</p>
 
-<h3>Zirkonya Kaplama Dişlerde İlerleyen Zamanlarda Renk Değişikliği Olur Mu?</h3>
+<h3><strong>Zirkonya Kaplama Dişlerde İlerleyen Zamanlarda Renk Değişikliği Olur Mu?</strong></h3>
 
 <p>Zirkonya kaplamaların rengi zamanla renklenmelere sebep olmaz. Günlük ağız hijyeni kurallarına uyulup, 6 ayda 1 düzenli diş hekimi kontrolüne gittikten sonra ekstra uzun yıllar ağızda kalır.&nbsp;</p>
 
-<h3>Zirkonya Dişlerde Kırılma Veya Kaplamanın Düşmesi Sık Görülür Mü?</h3>
+<h3><strong>Zirkonya Dişlerde Kırılma Veya Kaplamanın Düşmesi Sık Görülür Mü?</strong></h3>
 
 <p>Hastanın çiğneme hareketlerini, iki çenenin birbirine göre ilişkilerini doğru saptayarak yapılan tasarımların sonucunda rutin kullanımda bir kırılma beklenmez. Zirkonyanın kırılma direnci dişlerden daha yüksektir fakat şiddetli bir travma, kaza sonucu alınacak güçlü bir darbe dişin kırılmasına sebep olabilir.</p>
 
