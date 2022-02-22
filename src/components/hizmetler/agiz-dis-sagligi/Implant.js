35b8858665e5a73982d8bc958f7392a03fb1e9db
beratisahin <beratisahin94@gmail.com>
2022-02-23 01:55:14 +0300

    v1.0

--- src/components/hizmetler/agiz-dis-sagligi/Implant.js
+++ src/components/hizmetler/agiz-dis-sagligi/Implant.js
@@ -18,61 +18,61 @@ export default function Implant() {
                    
         <h4><strong>İmplant nedir?&nbsp;</strong></h4>
 
-<p>Dental implant; ağızda eksik dişlerin yerine çene kemiği içerisine yerleştirilen titanyum vidalardır. Bu vidaların üzerine yerleştirilen kron, köprü gibi hastaya uygun diş protezleri ile eksik dişlerin yeri doldurulur.</p>
+<p class="hizmetyazi">Dental implant; ağızda eksik dişlerin yerine çene kemiği içerisine yerleştirilen titanyum vidalardır. Bu vidaların üzerine yerleştirilen kron, köprü gibi hastaya uygun diş protezleri ile eksik dişlerin yeri doldurulur.</p>
 
 <h4><strong>Diş İmplantı nasıl uygulanır?</strong></h4>
 
-<p>İmplant yapılacak hastanın öncelikle genel sağlık durumu değerlendirilir ve ağız içi muayenesi yapılır. 3 boyutlu radyografik görüntüler ile mevcut kemiğin implant için uygunluğu değerlendirilir. Bu radyografi üzerinden milimetrik ölçümler yapılarak hangi uzunluk ve çapta implant uygulanacağına karar verilir. Ön planlama gerçekleştikten lokal anestezi altında işlem aşamasına geçilir. Çene kemiğinde implantlar için yuvalar açılır ve bu yuvalara implantlar yerleştirildikten sonra dikiş atılarak işlem sonlandırılır. Operasyondan ortalama 1 hafta sonra dikişler alınır ve kontrol muayenesi yapılır.&nbsp;</p>
+<p class="hizmetyazi">İmplant yapılacak hastanın öncelikle genel sağlık durumu değerlendirilir ve ağız içi muayenesi yapılır. 3 boyutlu radyografik görüntüler ile mevcut kemiğin implant için uygunluğu değerlendirilir. Bu radyografi üzerinden milimetrik ölçümler yapılarak hangi uzunluk ve çapta implant uygulanacağına karar verilir. Ön planlama gerçekleştikten lokal anestezi altında işlem aşamasına geçilir. Çene kemiğinde implantlar için yuvalar açılır ve bu yuvalara implantlar yerleştirildikten sonra dikiş atılarak işlem sonlandırılır. Operasyondan ortalama 1 hafta sonra dikişler alınır ve kontrol muayenesi yapılır.&nbsp;</p>
 
 
 <h4><strong>İmplant Tedavisinde İlk Muayenede Ne Yapılır?</strong></h4>
 
-<p>İlk muayenede gerekli klinik ve radyografik muayeneler ağız diş ve çene cerrahları tarafından yapılarak hastanın genel sağlık durumu değerlendirilir ve ağız içi muayenesi yapılır. Hastanın tedaviden beklentileri ve 3 boyutlu radyografik görüntüler ile mevcut kemiğin implant için uygunluğu değerlendirilir. Bu radyografi üzerinden milimetrik ölçümler yapılarak hangi uzunluk ve çapta implant uygulanacağına karar verilir. Estetik değerlendirmeler yapılarak hastaya en uygun üst yapı seçimi gerçekleştirilir.</p>
+<p class="hizmetyazi">İlk muayenede gerekli klinik ve radyografik muayeneler ağız diş ve çene cerrahları tarafından yapılarak hastanın genel sağlık durumu değerlendirilir ve ağız içi muayenesi yapılır. Hastanın tedaviden beklentileri ve 3 boyutlu radyografik görüntüler ile mevcut kemiğin implant için uygunluğu değerlendirilir. Bu radyografi üzerinden milimetrik ölçümler yapılarak hangi uzunluk ve çapta implant uygulanacağına karar verilir. Estetik değerlendirmeler yapılarak hastaya en uygun üst yapı seçimi gerçekleştirilir.</p>
 
 <h4><strong>Diş İmplantı Yaptırılmadan Önce Nelere Dikkat Edilmeli?</strong></h4>
 
-<p>Dental implant uygulaması cerrahi bir işlemdir. Alanında uzman ve yeterli donanıma sahip çene cerrahları tarafından uygulanması tedavi sonrasında yaşanacak geri dönüşü olmayan problemlere sebebiyet vermemek adına dikkat edilmesi gereken bir husustur.&nbsp;</p>
+<p class="hizmetyazi">Dental implant uygulaması cerrahi bir işlemdir. Alanında uzman ve yeterli donanıma sahip çene cerrahları tarafından uygulanması tedavi sonrasında yaşanacak geri dönüşü olmayan problemlere sebebiyet vermemek adına dikkat edilmesi gereken bir husustur.&nbsp;</p>
 
 <h4><strong>İmplant Uygulamasının Yararları Nelerdir?</strong></h4>
 
-<p>İmplant diş tedavisi ile yeterli sayıda destek dişi bulunmadığı için hareketli protez kullanmak zorunda kalan hastalara sabit protezler uygulanır. Komşu dişler kesilerek köprü uygulaması ile karşılaştırıldığında; implant tedavisi ile komşu dişlerin kesilmesine gerek kalmadan ağız içinde bulunan eksik diş tamamlanabilir. Hareketli protez kullanan ama kemik yetersizliğinden dolayı protezi tutmayan hastalarda implant yerleştirilip<a href="https://dentgroup.com.tr/tam-dis-protezleri/"> proteze</a> bağlanarak implant tutuculuğu artırıla bilinir. Dental implant;&nbsp;günümüzde diş eksikliklerinde kullanılan tedavi yöntemidir.</p>
+<p class="hizmetyazi">İmplant diş tedavisi ile yeterli sayıda destek dişi bulunmadığı için hareketli protez kullanmak zorunda kalan hastalara sabit protezler uygulanır. Komşu dişler kesilerek köprü uygulaması ile karşılaştırıldığında; implant tedavisi ile komşu dişlerin kesilmesine gerek kalmadan ağız içinde bulunan eksik diş tamamlanabilir. Hareketli protez kullanan ama kemik yetersizliğinden dolayı protezi tutmayan hastalarda implant yerleştirilip<a href="https://dentgroup.com.tr/tam-dis-protezleri/"> proteze</a> bağlanarak implant tutuculuğu artırıla bilinir. Dental implant;&nbsp;günümüzde diş eksikliklerinde kullanılan tedavi yöntemidir.</p>
 
 <h4><strong>İmplant Uygulaması Ne Kadar Sürer?</strong></h4>
 
-<p>İşlem süresi, uygulanacak implant sayısına göre farklılık gösterebilir. Ortalama implant başı 10 dakika gibi bir sürede cerrahlar tarafından yerleştirilir.</p>
+<p class="hizmetyazi">İşlem süresi, uygulanacak implant sayısına göre farklılık gösterebilir. Ortalama implant başı 10 dakika gibi bir sürede cerrahlar tarafından yerleştirilir.</p>
 
 
 
 <h4><strong>Her Hastaya İmplant Tedavisi Uygulanabilir mi?</strong></h4>
 
-<p>Kontrol altında olmayan önemli bir sistemik hastalığı olan, radyoterapi veya yakın dönemde kemoterapi alan bireylerde yapılması sakıncalıdır. Cerrahın implant yapılması ön görülen her hastanın sağlık geçmişini bilmesi bu sebep ile önemli bir husustur.&nbsp;</p>
+<p class="hizmetyazi">Kontrol altında olmayan önemli bir sistemik hastalığı olan, radyoterapi veya yakın dönemde kemoterapi alan bireylerde yapılması sakıncalıdır. Cerrahın implant yapılması ön görülen her hastanın sağlık geçmişini bilmesi bu sebep ile önemli bir husustur.&nbsp;</p>
 
 
 
 <h4><strong>İmplant Tedavisi İçin En Uygun Yaş Kaçtır?</strong></h4>
 
-<p>Büyüme gelişimi tamamlanmış ve sağlık durumu elverişli olan her hastaya implant tedavisi yapılabilmektedir. Kemik gelişiminin tamamlandığı yaş kadınlarda ortalama 15-16, erkeklerde ise 17-18 yaşlarıdır. Bu yaşlardan sonra her yaşta implant tedavisi yapılabilmektedir. Yapılan çalışmalara ve araştırmalara göre yaşlılık implant tedavisinde bir risk oluşturmamaktadır. Bu yüzden implant uygulamasında bir üst yaş sınırı yoktur.</p>
+<p class="hizmetyazi">Büyüme gelişimi tamamlanmış ve sağlık durumu elverişli olan her hastaya implant tedavisi yapılabilmektedir. Kemik gelişiminin tamamlandığı yaş kadınlarda ortalama 15-16, erkeklerde ise 17-18 yaşlarıdır. Bu yaşlardan sonra her yaşta implant tedavisi yapılabilmektedir. Yapılan çalışmalara ve araştırmalara göre yaşlılık implant tedavisinde bir risk oluşturmamaktadır. Bu yüzden implant uygulamasında bir üst yaş sınırı yoktur.</p>
 
 <h4><strong>Hangi Durumlarda İmplant Yapılmaz?</strong></h4>
 
-<p>Bazı sistemik rahatsızlıklar dental implant uygulamalarına engel olabilmektedir. Tedavi için başvurulan hekime sağlık durumunuz hakkında detaylı bilgi verilmelidir.</p>
+<p class="hizmetyazi">Bazı sistemik rahatsızlıklar dental implant uygulamalarına engel olabilmektedir. Tedavi için başvurulan hekime sağlık durumunuz hakkında detaylı bilgi verilmelidir.</p>
 
 <h4><strong>Sigara Kullanan Kişiler Dental İmplant Yaptırabilir mi?</strong></h4>
 
-<p>Yapılan birçok çalışmada sigaranın implant ile kemik bütünleşmesini engellediği kanıtlanmıştır. Sigara içen bireylerde, implantın düşme olasılığı normalden 2-3 kat daha yüksektir.</p>
+<p class="hizmetyazi">Yapılan birçok çalışmada sigaranın implant ile kemik bütünleşmesini engellediği kanıtlanmıştır. Sigara içen bireylerde, implantın düşme olasılığı normalden 2-3 kat daha yüksektir.</p>
 
 
 <h4><strong>Dental implantların İnsan Vücuduna Zarar Verme İhtimali Var Mıdır?&nbsp;</strong></h4>
 
-<p>Dental implantlar titanyumdan üretilmektedir. İnsan vücuduna uyumlu olan titanyum, herhangi bir alerji risk ve toksik barındırmayan metaldir.</p>
+<p class="hizmetyazi">Dental implantlar titanyumdan üretilmektedir. İnsan vücuduna uyumlu olan titanyum, herhangi bir alerji risk ve toksik barındırmayan metaldir.</p>
 
 <h4><strong>Dental İmplantlar Uzun Ömürlü müdür?</strong></h4>
 
-<p>Dental implantın ömrü doğal dişler gibidir. Standart ağız ve diş bakımı iyi yapıldığı sürece çok uzun yıllar kullanılır.</p>
+<p class="hizmetyazi">Dental implantın ömrü doğal dişler gibidir. Standart ağız ve diş bakımı iyi yapıldığı sürece çok uzun yıllar kullanılır.</p>
 
 <h4><strong>İmplant Tedavisi ortalama ücreti nedir?</strong></h4>
 
-<p>İmplant tedavi ücretleri kullanılan implant markasına göre değişkenlik gösterir. Hastaya uygun implant adet ve markası için hekim rendevusu gereklidir.&nbsp;</p>
+<p class="hizmetyazi">İmplant tedavi ücretleri kullanılan implant markasına göre değişkenlik gösterir. Hastaya uygun implant adet ve markası için hekim rendevusu gereklidir.&nbsp;</p>
 
 
 
